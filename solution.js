08c7204f41b1ec397f932ed81cce52be1cc8a233
Leo Volwahsen <volwahsenl@gmail.com>
2022-07-11 10:23:57 +0200

    switch

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
 // write your code here
+
+//1
+const color = "blue";
+
+switch (color) {
+    case "red":
+        console.log("red " + "Q1: Red's a great color on you.");
+        break;
+    case "blue":
+        console.log("blue " + "Q1: Blue? Great choice.");
+        break;
+    case "green":
+        console.log("green  " + "Q1: Ok. Green it is.");
+        break;
+    case "yellow":
+        console.log(
+            "yellow " + "Q1: Not what I would have picked, but yellow is fine."
+        );
+        break;
+    default:
+        console.log("Q1: The aim of the game is to pick a color.");
+}
